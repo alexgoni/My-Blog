4b49c9b2c04ccfd71975605f7b3761771a12d20b
alexgoni <goni000211@gmail.com>
2024-01-23 21:53:50 +0900

    feat: react-router-dom 설정

---
+++ src/components/Router.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "../pages/home";
+import About from "../pages/about";
+import Write from "../pages/write";
+import Edit from "../pages/edit";
+import Post from "../pages/post";
+import CategoryList from "../pages/category_list";
+
+export default function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category-list" element={<CategoryList />} />
+        <Route path="/category/:id" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
+  );
+}
