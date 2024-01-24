c9da79c8898e8272fdc1bdb0b7b1c73a342bc037
alexgoni <goni000211@gmail.com>
2024-01-24 20:22:34 +0900

    feat: category info list

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,40 +1,20 @@
 import Navbar from "components/Navbar";
 import React from "react";
+import Category from "./category";
+import CategoryInfo from "components/CategoryInfo";
+import PostList from "components/PostList";
 
 export default function Home() {
+  const categoryInfo = {
+    category: "All",
+    postNum: 0,
+  };
+
   return (
     <div>
       <Navbar />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        autem eveniet vel doloremque eligendi rerum repellendus provident odio
-        necessitatibus nulla suscipit similique nihil praesentium in
-        voluptatibus illum, sed, nobis aut!About
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        autem eveniet vel doloremque eligendi rerum repellendus provident odio
-        necessitatibus nulla suscipit similique nihil praesentium in
-        voluptatibus illum, sed, nobis aut!About
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        autem eveniet vel doloremque eligendi rerum repellendus provident odio
-        necessitatibus nulla suscipit similique nihil praesentium in
-        voluptatibus illum, sed, nobis aut!About
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        autem eveniet vel doloremque eligendi rerum repellendus provident odio
-        necessitatibus nulla suscipit similique nihil praesentium in
-        voluptatibus illum, sed, nobis aut!About
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        autem eveniet vel doloremque eligendi rerum repellendus provident odio
-        necessitatibus nulla suscipit similique nihil praesentium in
-        voluptatibus illum, sed, nobis aut!About
-      </p>
+      <CategoryInfo />
+      <PostList />
     </div>
   );
 }
