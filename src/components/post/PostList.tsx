6044507a6ce2e854e31f4cc8066b76e44261652d
alexgoni <goni000211@gmail.com>
2024-01-25 16:11:07 +0900

    feat: post 수정, 삭제 추가

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -22,7 +22,17 @@ function PostBlock() {
   return (
     <Link to="/post/1" className={styles.blockContainer}>
       <div className={styles.postBlock}>
-        <h1 className={styles.title}>{TITLE}</h1>
+        <div className={styles.postBlock__firstRow}>
+          <h1 className={styles.title}>{TITLE}</h1>
+          <div className={styles.utils}>
+            <Link to="/edit/1" className={styles.edit}>
+              수정
+            </Link>
+            <Link to="/edit/1" className={styles.delete}>
+              삭제
+            </Link>
+          </div>
+        </div>
         <div className={styles.introduction}>{INTRODUCTION}</div>
       </div>
     </Link>
