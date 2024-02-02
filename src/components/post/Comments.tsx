a5ca3f455411949be4e675dab13384d1581477cb
alexgoni <goni000211@gmail.com>
2024-02-02 14:55:43 +0900

    feat: 댓글 삭제 기능

--- src/components/post/Comments.tsx
+++ src/components/post/Comments.tsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import styles from "styles/post.module.scss";
 import { PostProps } from "./PostList";
 import { db } from "firebaseApp";
-import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { useRecoilValue } from "recoil";
 import { UserObjType, currentUserObj } from "recoil/user";
 import { toast } from "react-toastify";
 
-interface CommentFormProps {
+interface CommentLowerComponentProps {
   post: PostProps;
   user: UserObjType;
   getPost: (id: string) => Promise<void>;
@@ -20,7 +20,7 @@ export interface CommentsInterface {
   createdAt: string;
 }
 
-function CommentForm({ post, user, getPost }: CommentFormProps) {
+function CommentForm({ post, user, getPost }: CommentLowerComponentProps) {
   const [comment, setComment] = useState<string>("");
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -74,11 +74,20 @@ function CommentForm({ post, user, getPost }: CommentFormProps) {
   );
 }
 
-interface CommentListProps {
-  post: PostProps;
-}
+function CommentList({ post, user, getPost }: CommentLowerComponentProps) {
+  const handleDeleteComment = async (data: CommentsInterface) => {
+    const confirm = window.confirm("해당 댓글을 삭제하시겠습니까?");
+    if (confirm && post.id) {
+      const postRef = doc(db, "posts", post.id);
+      await updateDoc(postRef, {
+        comments: arrayRemove(data),
+      });
 
-function CommentList({ post }: CommentListProps) {
+      toast.success("댓글을 삭제했습니다.");
+      await getPost(post.id);
+    }
+  };
+
   return (
     <div className={styles.comments__list}>
       {post?.comments
@@ -89,7 +98,14 @@ function CommentList({ post }: CommentListProps) {
             <div className={styles.profileBox}>
               <div className={styles.email}>{comment?.email}</div>
               <div className={styles.date}>{comment?.createdAt}</div>
-              <div className={styles.delete}>삭제</div>
+              {user?.uid === comment.uid && (
+                <div
+                  className={styles.delete}
+                  onClick={() => handleDeleteComment(comment)}
+                >
+                  삭제
+                </div>
+              )}
             </div>
             <div className={styles.commentText}>{comment?.content}</div>
           </div>
@@ -109,7 +125,7 @@ export default function Comments({ post, getPost }: CommentsProps) {
   return (
     <div className={styles.comments}>
       {user && <CommentForm post={post} user={user} getPost={getPost} />}
-      <CommentList post={post} />
+      <CommentList post={post} user={user} getPost={getPost} />
     </div>
   );
 }
