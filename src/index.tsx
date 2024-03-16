4ca4521896deaa1ceca54cf083509119c72dac90
alexgoni <goni000211@gmail.com>
2024-03-16 18:17:09 +0900

    feat: link 이동 시 항상 스크롤 상단으로 고정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "styles/index.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import ScrollToTop from "modules/utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <RecoilRoot>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </RecoilRoot>
